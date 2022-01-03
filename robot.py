5f2d83340aa60aed502e836b633ff0ceff72f89c
caitevanish <cait.evan@gmail.com>
2022-01-03 13:00:22 -0800

    MVP for weapon.py is complete. Updated init method in robot class.

--- robot.py
+++ robot.py
@@ -6,15 +6,18 @@ class Robot:
     self.name = name
     self.attack_power = attack_power
     self.health = 100
-    pass
+    self.weapon1 = Weapon("Cannon", -20)
+    self.weapon2 = Weapon("Sword", -25)
+    self.weapon3 = Weapon("Morning Star", -30)
 
   def attack(self, dinosaur):
+    
     pass
 
 
-  def name_robot(self):
-    pass
+  # def name_robot(self):
+  #   pass
 
 
-  def health_robot(self):
-    pass
+  # def health_robot(self):
+  #   pass
--- weapon.py
+++ weapon.py
@@ -5,21 +5,13 @@ class Weapon:
     self.power = attack_power
 
   
-  # # I should take this out because the user cannot add their own weapons, right?:
+#   # I should take this out because the user cannot add their own weapons, right?:
 
-  # def add_weapon(self, name):
-  #   self.name = input("Add a new weapon: ")
-  #   return
-  #   pass
+#   def add_weapon(self, name):
+#     self.name = input("Add a new weapon: ")
+#     return
+#     pass
 
-  # def attack_power(self):
-    # self.power = 
-    # pass
-
-weapon1 = Weapon("Cannon", -20)
-weapon2 = Weapon("Sword", -25)
-weapon3 = Weapon("Morning Star", -30)
-
-# #Testing out whether I can use the debugger here (it didn't work):
-
-# Weapon.add_weapon(self, self.name)
+#   def attack_power(self):
+#     self.power = 
+#     pass
